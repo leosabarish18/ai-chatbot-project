74f330ac12d7f86b5f2dcbec550f137d5b23f324
Sabarish <arumugamsabari883@gmail.com>
2026-05-19 00:49:43 +0530

    Updated backend API URL

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -119,7 +119,7 @@ function App() {
     try {
 
       const response = await fetch(
-        "http://127.0.0.1:8000/chat",
+        "https://ai-chatbot-project-lmat.onrender.com/chat",
         {
           method: "POST",
           headers: {
